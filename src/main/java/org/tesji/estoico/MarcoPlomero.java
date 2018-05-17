363671f657e00417b7bcf2793bfcb764267b4503
Yael1SAY <yael.system10@gmail.com>
2018-05-16 21:07:25 -0500

    se agrega clase de Yael Estudiante

--- src/main/java/org/tesji/estoico/MarcoPlomero.java
+++ src/main/java/org/tesji/estoico/MarcoPlomero.java
@@ -3,8 +3,7 @@ package org.tesji.estoico;
 public class MarcoPlomero {
 	
 	private String especialidad;
-	
-	
+		
 	public void colocarLavavo() {
 		
 	}
---
+++ src/main/java/org/tesji/estoico/YaelEstudiante.java
@@ -0,0 +1,12 @@
+package org.tesji.estoico;
+
+public class YaelEstudiante {
+	
+	private String noCuenta;
+	private String carrera;
+	
+	public void ingrsar() {
+		
+	}
+
+}
